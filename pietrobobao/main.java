f40f012aa9ceac74c03c1eab9e779034e6e44dab
Pietro Tiago <166723178+PedPietro@users.noreply.github.com>
2025-11-24 15:32:06 -0300

    tentativa crud

--- pietrobobao/main.java
+++ pietrobobao/main.java
@@ -1,37 +1,298 @@
+ import com.mongodb.client.*;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoException;
-import com.mongodb.ServerApi;
-import com.mongodb.ServerApiVersion;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+import com.mongodb.client.result.DeleteResult;
+
+import com.mongodb.client.result.UpdateResult;
+
 import org.bson.Document;
 
-public class MongoClientConnectionExample {
+import org.bson.types.ObjectId;
+
+
+import java.util.Scanner;
+
+
+import static com.mongodb.client.model.Indexes.descending;     // para ordenação de resultados de consultas
+
+
+public class Main {
+
+
+    static Scanner leitor = new Scanner(System.in);
+
+
     public static void main(String[] args) {
-        String connectionString = "mongodb+srv://Pietro:<db_password>@daroca.o9szl1k.mongodb.net/?appName=DaRoca";
 
-        ServerApi serverApi = ServerApi.builder()
-                .version(ServerApiVersion.V1)
-                .build();
 
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(connectionString))
-                .serverApi(serverApi)
-                .build();
 
-        // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            try {
-                // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase("admin");
-                database.runCommand(new Document("ping", 1));
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } catch (MongoException e) {
-                e.printStackTrace();
+        // "mongodb+srv://nomeDoUsuario:senha@cluster0.zn5jm6v.mongodb.net/DaRoça?retryWrites=true&w=majority";
+
+        // @cluster0.zn5jm6v.mongodb.net é um endereço de exemplo de um BD no Atlas (em nuvem)
+
+        String uri = "mongodb+srv://Pietro:2910@DaRoca.mongodb.net/daroca?retryWrites=true&w=majority";
+
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+
+            MongoDatabase database = mongoClient.getDatabase("daroca");
+
+            MongoCollection<Document> aColecao = database.getCollection("categoria");     // corresponde a Tabela em SQL
+
+
+            // supomos que a coleção aberta possui campos _id (inteiro), campo1 (string), campo2 (double), ... até campoN (inteiro)
+
+            // o _id de cada documento incluído na coleção será gerado pelo código abaixo:
+
+
+            System.out.print("Deseja incluir um documento da coleção aberta? ");                   // documento da coleção corresponde a registro da tabela no SQL
+
+            String resposta = leitor.nextLine();
+
+            if (resposta.equalsIgnoreCase("S")) {
+
+
+                // fornecimento dos dados a serem armazenados no novo documento (registro)
+
+                System.out.print("Campo 1 :");
+
+                String campoString = leitor.nextLine();
+
+                System.out.print("Campo 2 :");
+
+                Double campoDouble = leitor.nextDouble();
+
+                // ...
+
+                System.out.print("Campo n:");
+
+                int campoInt = leitor.nextInt();
+
+
+                // Criando o próximo _id:
+
+
+                // Passo 1: Buscar o maior _id atual
+
+                Document ultimoId = aColecao.find()
+
+                        .sort(descending("_id"))
+
+                        .limit(1)
+
+                        .first();
+
+
+                // gera o próximo id
+
+                int proximoId = (ultimoId != null) ? ultimoId.getInteger("_id") + 1 : 1;
+
+
+                // gera o documento do novo produto:
+
+                Document novoDocumento = new Document("_id", proximoId).
+
+                         append("nomeDoCampo1", campoString).
+
+                         append("nomeDoCampo2", campoDouble)./* ... outros appends ... */
+
+                         append("nomeDoCampoN", campoInt);
+
+
+                // insere o documento na coleção
+
+                aColecao.insertOne(novoDocumento);
+
             }
+
+            else{
+
+                System.out.print("Deseja selecionar todos os registros? ");
+
+                String resposta2 = leitor.nextLine();
+
+                if (resposta2.equalsIgnoreCase("S")){
+
+                    MongoCursor<Document> cursor = aColecao.find().iterator();
+
+                    while (cursor.hasNext()) {
+
+                       Document doc = cursor.next();
+
+                        System.out.println(doc.get("nome") + ": " + doc.get("nota"));
+
+                    }
+
+                cursor.close();
+
+                }
+
+            }
+
+
+            System.out.print("Deseja excluir um documento? ");
+
+            resposta = leitor.nextLine();
+
+            if (resposta.equalsIgnoreCase("S")) {
+
+                System.out.print("Qual o id do documento a excluir na coleção aberta? ");
+
+                int idDocumento = leitor.nextInt();
+
+                Document filtro = new Document("_id", idDocumento);
+
+                DeleteResult resultado = aColecao.deleteOne(filtro);
+
+                if (resultado.getDeletedCount() > 0)
+
+                    System.out.println("Documento removido da coleção.");
+
+                else
+
+                    System.out.println("Documento não encontrado na coleção.");
+
+
+            }
+
+
+            System.out.print("Deseja alterar um documento? ");
+
+            resposta = leitor.nextLine();
+
+            if (resposta.equalsIgnoreCase("S")) {
+
+                System.out.print("Qual o id do documento a alterar na coleção aberta? ");
+
+                int idDocumento = leitor.nextInt();
+
+                Document filtro = new Document("_id", idDocumento);
+
+                FindIterable<Document> resultado = aColecao.find(filtro);
+
+                Document oDocumentoAAlterar = resultado.first();          // acessa o 1o documento do resultado pesquisado
+
+                if (oDocumentoAAlterar != null)
+
+                {
+
+                    String campoString = oDocumentoAAlterar.getString("nomeDoCampo1");
+
+                    double campoDouble = oDocumentoAAlterar.getDouble("nomeDoCampo2");
+
+                    /* ... solicitação de valores de outros campos */
+
+                    int campoInt = oDocumentoAAlterar.getInteger("nomeDoCampoN");
+
+
+
+                    System.out.println("Documento encontrado:");
+
+                    System.out.println("ID: " + oDocumentoAAlterar.getInteger("_id"));
+
+                    System.out.println("Campo 1: " + campoString);
+
+                    System.out.println("Campo 2: " + campoDouble);
+
+                    /* exibição de valores de outros campos */
+
+                    System.out.println("Campo N: " + campoInt);
+
+
+                    System.out.println("\nDigite os novos valores. Pressione [Enter] para manter o valor original\n");
+
+                    System.out.print("Novo Campo 1: ");
+
+                    campoString = leitor.nextLine();
+
+                    if (campoString.isEmpty())      // usuário teclou [Enter]
+
+                        campoString = oDocumentoAAlterar.getString("NomeDoCampo1");  // campoString recebe o valor original
+
+
+                    System.out.print("Novo Campo 2: ");
+
+                    String sCampoDouble = leitor.nextLine();
+
+                    if (!sCampoDouble.isEmpty())
+
+                        campoDouble = Double.parseDouble(sCampoDouble);
+
+
+                    /* ... digitação de novos calores dos demais campos ou [Enter] */
+
+                    System.out.print("Nova Campo N: ");
+
+                    String sCampoInt = leitor.nextLine();
+
+                    if (!sCampoInt.isEmpty())
+
+                        campoInt = Integer.parseInt(sCampoInt);
+
+
+                    Document documentoAlterado = new Document().append("_id", idDocumento).append("nomeCampo1", campoString).
+
+                                                                append("nomeCampo2", campoDouble). /* ... outros append("nome dos demais campos", novoValorDeCampo).  ... */
+
+                                                                append("nomeCampoN", campoInt);
+
+
+                    Document atualizacao = new Document("$set", documentoAlterado);
+
+
+                    UpdateResult resultadoUpdate = aColecao.updateOne(filtro, atualizacao);
+
+
+                    if (resultadoUpdate.getModifiedCount() > 0)
+
+                        System.out.println("Documento com _id " + idDocumento + " atualizado com sucesso.");
+
+                    else
+
+                        System.out.println("Documento não encontrado na coleção.");
+
+                }
+
+                else
+
+                    System.out.println("Nenhum produto encontrado com _id " + idDocumento);
+
+
+                // Exibir o conteúdo modificado da coleção aberta:
+
+
+                // Python
+
+                // for doc in osProdutos.find():
+
+                //      print(doc.toJson());
+
+
+                System.out.println("Id\tNome\tValor\tTipo");
+
+                for (Document doc : aColecao.find()) {
+
+                    System.out.println(doc.getInteger("_id")+"\t"+doc.getString("nomeDoCampo1")+"\t"+doc.getDouble("nomeDoCampo2")+"\t"+
+
+                            /*
+
+                            ... outros doc.getTipo("nome de campo") + ...
+
+                             */
+
+                            doc.getInteger("nomeDoCampoN"));
+
+                }
+
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
         }
+
     }
-}
+
+} 
